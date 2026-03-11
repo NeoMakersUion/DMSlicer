c9a102cd03a2004a758ec659c913ab3f148840ce
QilongJiang <ufo0001jione@gmail.com>
2026-03-11 12:53:49 +0800

    chore: 在 tools 模块中导出新增的工具函数
    新增 clean_dict、hex2rgb、hex2hsl、rgb2hex、hsl2hex 和 adjust_unit 函数到模块的公共接口，以便其他模块可以直接导入使用。

--- src/dmslicer/tools/__init__.py
+++ src/dmslicer/tools/__init__.py
@@ -2,7 +2,9 @@ from .hash_utils import calculate_hash, recursive_hashable
 from .process_bar import show_progress_bar, show_spinner
 from .progress_bar import RecursionSpinner
 from .size_of import get_total_size
-
+from .json2dict import clean_dict
+from .color_covertor import hex2rgb,hex2hsl,rgb2hex,hsl2hex
+from .unit_adjustor import adjust_unit
 __all__ = [
     "calculate_hash",
     "recursive_hashable",
@@ -10,4 +12,10 @@ __all__ = [
     "show_spinner",
     "RecursionSpinner",
     "get_total_size",
+    "clean_dict",
+    "hex2rgb",
+    "hex2hsl",
+    "rgb2hex",
+    "hsl2hex",
+    "adjust_unit"
 ]
