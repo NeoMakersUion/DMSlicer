b932cade186bd69061e9d83ba30cc07a80241f11
michael <ufo0001jone@gmail.com>
2026-03-15 22:21:53 +0800

    chore(tools): 更新模块导入和__all__列表
    添加新的调试和颜色工具函数，并调整导入格式以提高可读性

--- src/dmslicer/tools/__init__.py
+++ src/dmslicer/tools/__init__.py
@@ -3,7 +3,16 @@ from .process_bar import show_progress_bar, show_spinner
 from .progress_bar import RecursionSpinner
 from .size_of import get_total_size
 from .json2dict import clean_dict
-from .color_covertor import hex2rgb,hex2hsl,rgb2hex,hsl2hex
+from .color_covertor import (
+    hex2rgb,
+    hex2hsl,
+    rgb2hex,
+    hsl2hex,
+    normalize_rgb,
+    rgb_to_hex,
+    format_rgbarray,
+)
+from .debug_render import object_id_badge_svg, color_swatch_html
 from .unit_adjustor import adjust_unit
 __all__ = [
     "calculate_hash",
@@ -17,5 +26,10 @@ __all__ = [
     "hex2hsl",
     "rgb2hex",
     "hsl2hex",
-    "adjust_unit"
+    "normalize_rgb",
+    "rgb_to_hex",
+    "format_rgbarray",
+    "object_id_badge_svg",
+    "color_swatch_html",
+    "adjust_unit",
 ]
